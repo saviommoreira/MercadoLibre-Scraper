f28d64cb524df6e47ee9cb1538e4ab7cc6003dec
Savio Moreira <saviommoreira@gmail.com>
2024-11-27 22:04:30 -0300

    get new info and ajust format to utf-8

--- main.py
+++ main.py
@@ -1,72 +1,48 @@
-import requests
-from bs4 import BeautifulSoup
-import pandas as pd
 import os
+import requests
+import pandas as pd
+from bs4 import BeautifulSoup
 
 class Scraper():
 
     def menu(self):
         menu = ("""
-    Escoge el país:
-    1. Argentina
-    2. Bolivia
-    3. Brasil
-    4. Chile
-    5. Colombia
-    6. Costa Rica
-    7. Dominicana
-    8. Ecuador
-    9. Guatemala
-    10. Honduras
-    11. México
-    12. Nicaragua
-    13. Panamá
-    14. Paraguay
-    15. Perú
-    16. Salvador
-    17. Uruguay
-    18. Venezuela
+    Ecolha o país:
+    1. Brasil
         """)
 
         valid_options = list(range(1, 19))
 
         while True:
             print(menu)
-            opcion = int(input('Número de país (Ejemplo: 5): '))
+            opcion = int(input('Número de país (Exemplo: 1): '))
 
             if opcion in valid_options:
-
                 urls = {
-                1: 'https://listado.mercadolibre.com.ar/',
-                2: 'https://listado.mercadolibre.com.bo/',
-                3: 'https://listado.mercadolibre.com.br/',
-                4: 'https://listado.mercadolibre.cl/',
-                5: 'https://listado.mercadolibre.com.co/',
-                6: 'https://listado.mercadolibre.com.cr/',
-                7: 'https://listado.mercadolibre.com.do/',
-                8: 'https://listado.mercadolibre.com.ec/',
-                9: 'https://listado.mercadolibre.com.gt/',
-                10: 'https://listado.mercadolibre.com.hn/',
-                11: 'https://listado.mercadolibre.com.mx/',
-                12: 'https://listado.mercadolibre.com.ni/',
-                13: 'https://listado.mercadolibre.com.pa/',
-                14: 'https://listado.mercadolibre.com.py/',
-                15: 'https://listado.mercadolibre.com.pe/',
-                16: 'https://listado.mercadolibre.com.sv/',
-                17: 'https://listado.mercadolibre.com.uy/',
-                18: 'https://listado.mercadolibre.com.ve/',
+                1: 'https://lista.mercadolivre.com.br/',
                 }
-
                 self.base_url = urls[opcion]
                 break
             else:
-                print("Escoge un número del 1 al 18")
+                print("Escolha um número de 1 a 1")
 
+    def convert_to_float(self, price_str):
+        """Converte uma string de preço para float"""
+        if not price_str:
+            return 0.0
+        try:
+            price_cleaned = price_str.replace("R$", "").replace("$", "").replace(".", "").replace(",", ".").strip()
+            return float(price_cleaned)
+        except ValueError:
+            return 0.0
 
-
+    def format_to_currency(self, value):
+        """Formata um valor float para o formato de moeda brasileiro"""
+        return f"R$ {value:,.2f}".replace(",", "X").replace(".", ",").replace("X", ".")
+    
     def scraping(self):
         # User search
-        product_name = input("\nProducto: ")
+        product_name = input("\nDigite o produto: ")
         # Clean the user input
         cleaned_name = product_name.replace(" ", "-").lower()
         # Create the urls to scrap
@@ -91,23 +67,56 @@ class Scraper():
                 
             # take all posts
             content = soup.find_all('li', class_='ui-search-layout__item')
+            #print(content)
             
             # Check if there's no content to scrape
             if not content:
-                print("\nTermino el scraping.")
+                print("\nTérmino do scraping.")
                 break
 
-            print(f"\nScrapeando pagina numero {i}. {url}")
-            
+            print(f"\nScrapeando página número {i}. {url}")
             
             # iteration to scrape posts
             for post in content:
                 # get the title
-                title = post.find('h2').text
-                # get the price
-                price = post.find('span', class_='andes-money-amount__fraction').text
+                title = post.find('h2', class_='poly-box poly-component__title').text
+
+                # get the previous price
+                price_previous = post.find('s', class_='andes-money-amount andes-money-amount--previous andes-money-amount--cents-comma')
+                if price_previous:
+                    price_previous = price_previous.text
+                else:
+                    price_previous = post.find('span', class_='andes-money-amount__fraction').text
+                
+                # Convert and format the previous price
+                price_previous = self.format_to_currency(self.convert_to_float(price_previous))
+
+                # get the current price
+                price_current = post.find('span', class_='andes-money-amount andes-money-amount--cents-superscript').text
+                # Convert and format the current price
+                price_current = self.format_to_currency(self.convert_to_float(price_current))
+
+                # get discount
+                discount = post.find('span', class_='andes-money-amount__discount')
+                discount = discount.get_text().strip() if discount else "0%"
+
+                # get seller
+                seller = post.find('span', class_='poly-component__seller')
+                seller = seller.get_text().strip() if seller else "N/A"
+
+                # get installments
+                installments = post.find('span', class_='poly-price__installments poly-text-positive')
+                if installments:
+                    installments = installments.get_text().strip()
+                    ad_type = 'premium'
+                else:
+                    installments = post.find('span', class_='poly-price__installments poly-text-primary')
+                    installments = installments.get_text().strip() if installments else "N/A"
+                    ad_type = 'classic'
+
                 # get the url post
                 post_link = post.find("a")["href"]
+
                 # get the url image
                 try:
                     img_link = post.find("img")["data-src"]
@@ -115,12 +124,17 @@ class Scraper():
                     img_link = post.find("img")["src"]
                 
                 # show the data already scraped
-                # print(f"{c}. {title}, {price}, {post_link}, {img_link}")
+                # print(f"{c}. {title}, {price}, {discount}, {post_link}, {img_link}")
 
                 # save in a dictionary
                 post_data = {
                     "title": title,
-                    "price": price,
+                    "price_previous": price_previous,
+                    "price_current": price_current,
+                    "discount": discount,
+                    "installments": installments,
+                    "ad_type:": ad_type, 
+                    "seller": seller,
                     "post link": post_link,
                     "image link": img_link            
                 }
@@ -129,9 +143,13 @@ class Scraper():
                 c += 1
 
     def export_to_csv(self):
-        # export to a csv file
+        # Cria a pasta "data" se ela não existir
+        os.makedirs("data", exist_ok=True)
+        
+        # Exporta para um arquivo CSV em UTF-8
         df = pd.DataFrame(self.data)
-        df.to_csv(r"data/mercadolibre_scraped_data.csv", sep=";")
+        df.to_csv(r"data/mercadolibre_scraped_data.csv", sep=";", encoding="utf-8-sig", index=False)
+        print("Arquivo CSV exportado com sucesso em UTF-8!")
 
 if __name__ == "__main__":
     s = Scraper()
